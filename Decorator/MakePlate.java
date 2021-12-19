013750a1a340721540b93fea57da1c357be55116
Conor Hartigan <Conor.Hartigan2@students.ittralee.ie>
2021-12-19 19:15:37 +0000

    Added test class for decorator

--- Decorator/MakePlate.java
+++ Decorator/MakePlate.java
@@ -1,8 +1,9 @@
 public class MakePlate {
     public static void main(String[] args) {
 
-        Plate plate = new Chips((new Salmon()));
+        Plate plate = (new Salmon());
+        plate = new Chips (plate);
 
-        System.out.println(plate.getDescription());
+        System.out.println("Items on plate: " + plate.getDescription() + ", Total: " + plate.price());
     }
 }
